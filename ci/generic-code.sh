76b1064bd956953fce2e48c77ae8c4df95827322
Guilherme Cartier <gcartier@kyndryl.com>
2021-11-01 16:59:32 -0300

    ci: Includes USS use case
    Signed-off-by: Guilherme Cartier <gcartier@kyndryl.com>

---
+++ ci/generic-code.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Generic CI code to trigger actions in Unix System Services"
+
+uname -a 
+
+echo "Congratulations, you just executed a remote script on USS using Github Actions!"
+
+echo "Bye!"
